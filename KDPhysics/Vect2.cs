36dbc063f93a34ff28c4d66f6c17a82a2df58447
Calvin Wilkinson <calvin.wilkinson@ferox-consulting.com>
2017-03-16 18:26:36 -0400

    Create a vector structure
    This is to hold the magnitue and direction in 2D space.

---
+++ KDPhysics/Vect2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDPhysics
+{
+    /// <summary>
+    /// Represents direction and magnitude in 2D space.
+    /// </summary>
+    public struct Vect2
+    {
+        /// <summary>
+        /// Gets or sets the X coordinate of the vector.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y coordinate of the vector.
+        /// </summary>
+        public int Y { get; set; }
+    }
+}
