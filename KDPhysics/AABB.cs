2999e688d3ac9c56c64e646083df27dc8eb39ea5
Calvin Wilkinson <calvin.wilkinson@ferox-consulting.com>
2017-03-16 18:30:49 -0400

    Create a axis aligned bounding box struct
    This is to represent a rigid axis aligned bounding box body for broad phase collision detection.

---
+++ KDPhysics/AABB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDPhysics
+{
+    /// <summary>
+    /// Represents an Axis Aligned Bounding Box.  Main shape used to perform broad phase collision detection.
+    /// </summary>
+    public struct AABB
+    {
+        /// <summary>
+        /// Represents the top left corner of the bounding rectangle.
+        /// </summary>
+        public Vect2 Min { get; set; }
+
+        /// <summary>
+        /// Represents the bottom right corner of the bounding rectangle.
+        /// </summary>
+        public Vect2 Max { get; set; }
+    }
+}
