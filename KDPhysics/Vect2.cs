ed666deca48b4260065cdb4f8c5edb8fcbdfe6d3
Calvin Wilkinson <calvin.wilkinson@ferox-consulting.com>
2017-03-16 19:49:29 -0400

    Create method to calculate dot product.

--- KDPhysics/Vect2.cs
+++ KDPhysics/Vect2.cs
@@ -20,5 +20,17 @@ namespace KDPhysics
         /// Gets or sets the Y coordinate of the vector.
         /// </summary>
         public int Y { get; set; }
+        /// <summary>
+        /// Calculates the dot product of the given vectors.
+        /// </summary>
+        /// <param name="start">The starting vector.</param>
+        /// <param name="end">The ending vector.</param>
+        /// <returns>The scalar dot product value of the start and end vectors.</returns>
+        public static int DotProduct (Vect2 start, Vect2 end)
+        {
+            //Dot Product Ref: https://www.mathsisfun.com/algebra/vectors-dot-product.html
+
+            return (start.X * end.X) + (start.Y * end.Y);
+        }
     }
 }
