c27980f664fa14fc5d70da3431aa0238d386fc99
Calvin Wilkinson <calvin.wilkinson@ferox-consulting.com>
2017-04-13 20:18:30 -0400

    Add label capability to Axis class.

--- KDPhysicsTestGame/Axis.cs
+++ KDPhysicsTestGame/Axis.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace KDPhysicsTestGame
@@ -14,24 +15,32 @@ namespace KDPhysicsTestGame
     public class Axis
     {
         private Texture2D _texture;
+        private SpriteFont _font;
+        private Vector2 _labelPosition;
+
+
 
         /// <summary>
         /// Creates a new instance of Axis.
         /// </summary>
         /// <param name="graphicsDevice">The graphics device to use to render the PhysObj.</param>
+        /// <param name="content">The content manager used to load the content.</param>
         /// <param name="type">The type of axis.</param>
         /// <param name="position">The position to draw the axis.</param>
         /// <param name="length">The length in pixels of the axis.</param>
         /// <param name="label">The label of the axis.</param>
         /// <param name="axisColor">The color of the axis.</param>
         /// <param name="labelColor">The color of the axis label.</param>
-        public Axis(GraphicsDevice graphicsDevice, AxisType type, Vector2 position, int length, string label, Color axisColor, Color labelColor)
+        public Axis(GraphicsDevice graphicsDevice, ContentManager content, AxisType type, Vector2 position, int length, string label, Color axisColor, Color labelColor)
         {
+            _font = content.Load<SpriteFont>("Font/arial-36");
+
             switch (type)
             {
                 case AxisType.XAxis:
                     _texture = new Texture2D(graphicsDevice, length, 2);
                     _texture.SetAsSolid(length, 2, axisColor);
+                    _labelPosition = new Vector2(length / 2 + position.X - _font.MeasureString(label).X / 2, position.Y + 5);
                     break;
                 case AxisType.YAxis:
                     _texture = new Texture2D(graphicsDevice, 2, length);
@@ -80,6 +89,9 @@ namespace KDPhysicsTestGame
         {
             //Render the axis
             spriteBatch.Draw(_texture, Position, AxisColor);
+
+            //Render the label text
+            spriteBatch.DrawString(_font, Label, _labelPosition, LabelColor);
         }
     }
 }
