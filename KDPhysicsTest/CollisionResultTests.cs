b6d0db3aa2af111506eb022b3b399ddc275a869d
Calvin Wilkinson <calvin.wilkinson@ferox-consulting.com>
2017-05-15 20:53:49 -0400

    Create a unit test to test the CollisionResult struct.

---
+++ KDPhysicsTest/CollisionResultTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.CodeDom;
+using KDPhysics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable UseObjectOrCollectionInitializer
+
+namespace KDPhysicsTest
+{
+    [TestClass]
+    public class CollisionResultTests
+    {
+        [TestMethod]
+        public void WillIntersect_Prop_Set_Valid()
+        {
+            //Arrange
+            var collisionResult = new CollisionResult();
+
+            //Act
+            collisionResult.WillIntersect = true;
+
+            //Assert
+            Assert.AreEqual(true, collisionResult.WillIntersect);
+        }
+
+        [TestMethod]
+        public void Intersects_Prop_Set_Valid()
+        {
+            //Arrange
+            var collisionResult = new CollisionResult();
+
+            //Act
+            collisionResult.Intersects = true;
+
+            //Assert
+            Assert.AreEqual(true, collisionResult.Intersects);
+        }
+
+        [TestMethod]
+        public void MinTranslationVector_Prop_Set_Valid()
+        {
+            //Arrange
+            var collisionResult = new CollisionResult();
+            
+            //Act
+            collisionResult.MinTranslationVector = new Vect2(12, 45);
+
+            //Assert
+            Assert.AreEqual(new Vect2(12, 45), collisionResult.MinTranslationVector);
+        }
+    }
+}
